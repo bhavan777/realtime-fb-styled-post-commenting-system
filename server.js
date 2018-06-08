1a14b0143e1fe8daf10949f1c17416c523907e3e
Bhavan Kuchibotla <you@example.com>
2018-06-08 19:15:05 +0530

    local sockets initialised with post and comment receival

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-
+var path = require('path');
 // our localhost port
-const port = 4001;
+const port = 4000;
 
 const app = express();
 
@@ -16,11 +16,22 @@ const io = socketIO(server);
 
 // This is what the socket.io syntax is like, we will work this later
 io.on('connection', socket => {
-  console.log('User connected');
+  console.log('new connecion');
+  io.sockets.emit('welcome', 'yo client welcome');
+
+  socket.on('post', (post) => {
+    console.log('new post added', post);
+  });
+  socket.on('comment', (data) => {
+    console.log('new comment added', data.comment, data.postId);
+  });
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'src/index.html'));
+});
 server.listen(port, () => console.log(`Listening on port ${port}`));
